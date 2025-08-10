6c27c460e564bb630ef10b0115631e1ee1d3310e
Sneha Vaish <126096173+sneha01vaish@users.noreply.github.com>
2025-08-11 00:41:40 +0530

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,17 +2,17 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDPgXNERKPxpf5HzYas1qvfUF29mBfHd-o",
-  authDomain: "canvaseditor-8162e.firebaseapp.com",
-  projectId: "canvaseditor-8162e",
-  storageBucket: "canvaseditor-8162e.firebasestorage.app",
-  messagingSenderId: "676598600175",
-  appId: "1:676598600175:web:b2d201a947777cb24be61a",
-  measurementId: "G-X1087D4CM0"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+
 
 export default app;
